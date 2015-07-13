7ea8e0a63bc760832c64dbc32bf078678db49196
jredondo <jredondo@canalcom.com>
2015-07-13 12:42:44 +0200

    Eliminado error en la carga inicial del mapa

--- Bosques/platforms/android/assets/www/js/controllers/map.js
+++ Bosques/platforms/android/assets/www/js/controllers/map.js
@@ -15,66 +15,33 @@ angular.module('starter.controllers.map', [])
       $ionicPopup, 
       SharePathService
       ) {
-        SharePathService.prepForBroadcast('', '');
+      
+        var map, greenIcon, nowIcon;
+        var firstRun = true;
+  
+        $scope.$on('$ionicView.enter', function () {
+            // Code you want executed every time view is opened
+            console.log('Opened!');
+            if (firstRun) {
+                $scope.init();
+                firstRun = false;
+            }
+        })
 
-        var nowIcon = L.icon({
-            iconUrl: 'img/map-marker.png',
-            iconSize: [64, 64], // size of the icon
-//            shadowSize: [50, 64], // size of the shadow
-            iconAnchor: [32, 64], // point of the icon which will correspond to marker's location
-//            shadowAnchor: [4, 62],  // the same for the shadow
-            popupAnchor: [-3, -64] // point from which the popup should open relative to the iconAnchor*/
+        $scope.$on("pathChange", function (event) {
+            $scope.pathChange();
         });
-
-        var greenIcon = L.icon({
-            iconUrl: 'img/leaf-green.png',
-            shadowUrl: 'img/leaf-shadow.png',
-
-            iconSize: [20, 45], // size of the icon
-            shadowSize: [20, 30], // size of the shadow
-            iconAnchor: [10, 45], // point of the icon which will correspond to marker's location
-            shadowAnchor: [4, 30],  // the same for the shadow
-            popupAnchor: [-3, -45] // point from which the popup should open relative to the iconAnchor
-        });
-
-        var map = L.map('map', {
-            center: [40.5, -2], //[position.coords.latitude, position.coords.longitude],
-            zoom: 15
-        }
-                );
-        L.tileLayer('http://jjshaw.atwebpages.com/maverick/raster.php?z={z}&x={x}&y={y}', {
-            maxZoom: 18
-        }).addTo(map);
 
         /**
-        * Once state loaded, get put map on scope.
-        */
+       * Once state loaded, get put map on scope.
+       */
         $scope.$on("$stateChangeSuccess", function () {
              $scope.locate();
         });
 
-        $scope.$on("pathChange", function (event) {
-            if(SharePathService.path!=''){
-                new L.GPX(SharePathService.path, {
-                                                    async: true,
-                                                    marker_options: {
-                                                        startIconUrl: 'img/pin-icon-start.png',
-                                                        endIconUrl: 'img/pin-icon-end.png',
-                                                        shadowUrl: 'img/pin-shadow.png'
-                                                    }
-                                                })
-                                             .on('loaded', function (e) {
-                                                 map.fitBounds(e.target.getBounds());
-                                             })
-                           .addTo(map);
+       
 
-                SharePathService.pois.forEach(function (poi) {
-                    L.marker([poi.lat, poi.lng], { icon: greenIcon, title: poi.title })
-                        .addTo(map);
-                    //.on('click', onClickMarker(poi.title, poi.img, poi.text));
-                });
-            }
-        });
+               
 
         /*function onClickMarker(title, img, text){
             $ionicPopup.show({
@@ -93,6 +60,7 @@ angular.module('starter.controllers.map', [])
          /**
          * Center map on user's current position and load GPX
          */
+
         $scope.locate = function () {
            $cordovaGeolocation
               .getCurrentPosition()
@@ -113,6 +81,64 @@ angular.module('starter.controllers.map', [])
                   console.log(err);
               });
 
+        };
+
+        $scope.init = function () {
+            nowIcon = L.icon({
+                iconUrl: 'img/map-marker.png',
+                iconSize: [64, 64], // size of the icon
+                //            shadowSize: [50, 64], // size of the shadow
+                iconAnchor: [32, 64], // point of the icon which will correspond to marker's location
+                //            shadowAnchor: [4, 62],  // the same for the shadow
+                popupAnchor: [-3, -64] // point from which the popup should open relative to the iconAnchor*/
+            });
+
+            greenIcon = L.icon({
+                iconUrl: 'img/leaf-green.png',
+                shadowUrl: 'img/leaf-shadow.png',
+
+                iconSize: [20, 45], // size of the icon
+                shadowSize: [20, 30], // size of the shadow
+                iconAnchor: [10, 45], // point of the icon which will correspond to marker's location
+                shadowAnchor: [4, 30],  // the same for the shadow
+                popupAnchor: [-3, -45] // point from which the popup should open relative to the iconAnchor
+            });
+
+            map = L.map('map', {
+                center: [40.5, -2], //[position.coords.latitude, position.coords.longitude],
+                zoom: 15
+            }
+                    );
+
+            L.tileLayer('http://jjshaw.atwebpages.com/maverick/raster.php?z={z}&x={x}&y={y}', {
+                maxZoom: 18
+            }).addTo(map);
+
+            $scope.locate();
+            $scope.pathChange();
+        };
+
+        $scope.pathChange = function () {
+            if (SharePathService.path != '') {
+                new L.GPX(SharePathService.path, {
+                    async: true,
+                    marker_options: {
+                        startIconUrl: 'img/pin-icon-start.png',
+                        endIconUrl: 'img/pin-icon-end.png',
+                        shadowUrl: 'img/pin-shadow.png'
+                    }
+                })
+                                             .on('loaded', function (e) {
+                                                 map.fitBounds(e.target.getBounds());
+                                             })
+                           .addTo(map);
+
+                SharePathService.pois.forEach(function (poi) {
+                    L.marker([poi.lat, poi.lng], { icon: greenIcon, title: poi.title })
+                        .addTo(map);
+                    //.on('click', onClickMarker(poi.title, poi.img, poi.text));
+                });
+            }
         };
 
     }]);
--- Bosques/www/js/controllers/path.js
+++ Bosques/www/js/controllers/path.js
@@ -4,9 +4,11 @@ angular.module('starter.controllers.path', [])
     $scope.paths = PathsFactory.all();
 })
 
- .controller('PathDetailCtrl', function ($scope, $stateParams, PathsFactory, SharePathService) {
+ .controller('PathDetailCtrl', function ($scope, $stateParams, $state, PathsFactory, SharePathService) {
      $scope.path = PathsFactory.get($stateParams.pathId);
      $scope.goPath = function (gpx, pois) {
+         //ui-sref="tab.map"
          SharePathService.prepForBroadcast(gpx, pois);
+         $state.go("tab.map"); $state.go("tab.path"); $state.go("tab.map");
      }
  });
